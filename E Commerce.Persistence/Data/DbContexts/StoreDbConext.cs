2dfbd721c297caa20b21df56004fd8a7dad74c5c
MohamedBesher <103468249+BeshOOo1@users.noreply.github.com>
2025-11-01 02:39:29 +0200

    Add project files.

---
+++ E Commerce.Persistence/Data/DbContexts/StoreDbConext.cs
@@ -0,0 +1,30 @@
+﻿using E_Commerce.Domain.Entities.ProductModule;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Data.DbContexts
+{
+    public class StoreDbConext : DbContext
+    {
+        public StoreDbConext(DbContextOptions<StoreDbConext> options): base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+
+        #region Dbsets
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        #endregion
+    }
+}
